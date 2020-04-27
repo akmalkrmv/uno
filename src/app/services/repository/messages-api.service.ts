b6aadae1253cd2fa4c80f6234f97a1149e24f9b6
akmalkrmv <akmalkarimov94@gmail.com>
2020-04-27 16:51:42 +0300

    removed firedatabse

--- src/app/services/repository/messages-api.service.ts
+++ src/app/services/repository/messages-api.service.ts
@@ -7,28 +7,15 @@ import { Observable } from 'rxjs';
 import { BaseFirestoreService } from './base-firestore.service';
 import { Message } from '@models/index';
 
-import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
-import { map } from 'rxjs/operators';
-
 @Injectable({ providedIn: 'root' })
 export class MessagesApiService extends BaseFirestoreService {
   public collection: AngularFirestoreCollection<Message>;
   public messages$: Observable<Message[]>;
 
-  public messageList: AngularFireList<Message>;
-
-  constructor(
-    private firestore: AngularFirestore,
-    private firedatabase: AngularFireDatabase
-  ) {
+  constructor(private firestore: AngularFirestore) {
     super();
     this.collection = firestore.collection<Message>('messages');
-    this.messageList = firedatabase.list<Message>('/messages');
-
     this.messages$ = this.collectionChanges(this.collection);
-    // this.messages$ = this.messageList
-    //   .snapshotChanges()
-    //   .pipe(map((changes) => changes.map((change) => {id: change.key})));
   }
 
   public create(payload: Message) {
--- src/app/shared/firestore-imports.module.ts
+++ src/app/shared/firestore-imports.module.ts
@@ -3,7 +3,6 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireMessagingModule } from '@angular/fire/messaging';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { environment } from 'src/environments/environment';
 
 @NgModule({
@@ -11,14 +10,12 @@ import { environment } from 'src/environments/environment';
     AngularFirestoreModule,
     AngularFireAuthModule,
     AngularFireMessagingModule,
-    AngularFireDatabaseModule,
     AngularFireModule.initializeApp(environment.firebase),
   ],
   exports: [
     AngularFirestoreModule,
     AngularFireAuthModule,
     AngularFireMessagingModule,
-    AngularFireDatabaseModule
   ],
 })
 export class FirestoreImportsModule {}
