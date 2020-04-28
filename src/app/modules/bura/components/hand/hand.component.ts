f83c6b598d71da9f61c45fc6442fa6d52cb6ec47
akmalkrmv <akmalkarimov94@gmail.com>
2020-04-28 23:37:54 +0300

    hand component inputs

--- src/app/modules/bura/components/hand/hand.component.ts
+++ src/app/modules/bura/components/hand/hand.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Player } from '../../models/player';
+import { Card } from '../../models/card.model';
+import { GameState } from '../../models/game-state';
 
 @Component({
   selector: 'app-hand',
   templateUrl: './hand.component.html',
-  styleUrls: ['./hand.component.scss']
+  styleUrls: ['./hand.component.scss'],
 })
 export class HandComponent implements OnInit {
+  @Input() player: Player;
+  @Input() current: Player;
+  @Input() isBeater: boolean;
+  @Input() isCurrent: boolean;
+  @Input() beatingCards: Card[];
+  @Input() state: GameState;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
