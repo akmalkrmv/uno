0330d44edba2db0e066e14ac35f88c0d2c5927e4
akmalkrmv <akmalkarimov94@gmail.com>
2020-05-01 23:53:12 +0300

    refactor: sort imports

--- src/app/modules/video-chat/video-chat.module.ts
+++ src/app/modules/video-chat/video-chat.module.ts
@@ -1,28 +1,28 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 
-import { RoomComponent } from './room/room.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { RoomToolbarComponent } from './room-toolbar/room-toolbar.component';
-import { VideosListComponent } from './videos-list/videos-list.component';
-import { VideoControlsComponent } from './video-controls/video-controls.component';
 import { CallDialogComponent } from './call-dialog/call-dialog.component';
-import { VideoItemComponent } from './video-item/video-item.component';
-import { SelfVideoComponent } from './self-video/self-video.component';
+import { RoomComponent } from './room/room.component';
 import { RoomControlsComponent } from './room-controls/room-controls.component';
+import { RoomToolbarComponent } from './room-toolbar/room-toolbar.component';
+import { SelfVideoComponent } from './self-video/self-video.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { VideoControlsComponent } from './video-controls/video-controls.component';
+import { VideoItemComponent } from './video-item/video-item.component';
+import { VideosListComponent } from './videos-list/videos-list.component';
 
 @NgModule({
   entryComponents: [CallDialogComponent],
   declarations: [
-    RoomComponent,
-    UserListComponent,
-    RoomToolbarComponent,
-    VideosListComponent,
-    VideoControlsComponent,
     CallDialogComponent,
-    VideoItemComponent,
-    SelfVideoComponent,
+    RoomComponent,
     RoomControlsComponent,
+    RoomToolbarComponent,
+    SelfVideoComponent,
+    UserListComponent,
+    VideoControlsComponent,
+    VideoItemComponent,
+    VideosListComponent,
   ],
   imports: [SharedModule],
 })
