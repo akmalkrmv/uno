773bc1c47cd227dfb4d6c6113d7931abe1bf2d5d
akmalkrmv <akmalkarimov94@gmail.com>
2020-05-03 20:36:53 +0300

    Updating module

--- src/app/modules/video-chat/video-chat.module.ts
+++ src/app/modules/video-chat/video-chat.module.ts
@@ -3,9 +3,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
 
 import { CallDialogComponent } from './components/call-dialog/call-dialog.component';
 import { RoomComponent } from './components/room/room.component';
-import { RoomControlsComponent } from './components/room-controls/room-controls.component';
 import { RoomListComponent } from './components/room-list/room-list.component';
-import { RoomToolbarComponent } from './components/room-toolbar/room-toolbar.component';
 import { SelfVideoComponent } from './components/self-video/self-video.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { VideoControlsComponent } from './components/video-controls/video-controls.component';
@@ -17,9 +15,7 @@ import { VideosListComponent } from './components/videos-list/videos-list.compon
   declarations: [
     CallDialogComponent,
     RoomComponent,
-    RoomControlsComponent,
     RoomListComponent,
-    RoomToolbarComponent,
     SelfVideoComponent,
     UserListComponent,
     VideoControlsComponent,
