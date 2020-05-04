d5fdd0978fd0ae968123c4a414d88eb76cba8811
akmalkrmv <akmalkarimov94@gmail.com>
2020-05-04 15:00:01 +0300

    using environment keys

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -8,16 +8,7 @@ importScripts(
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
-firebase.initializeApp({
-  apiKey: 'AIzaSyB8OELF8OtaSi-hF8rJm3lzgBn5GOI6Yeo',
-  authDomain: 'uno-server-3000.firebaseapp.com',
-  databaseURL: 'https://uno-server-3000.firebaseio.com',
-  projectId: 'uno-server-3000',
-  storageBucket: 'uno-server-3000.appspot.com',
-  messagingSenderId: '460750590702',
-  appId: '1:460750590702:web:e38380af9f7ff3e2a699ff',
-  measurementId: 'G-1D1LTS2GWF',
-});
+firebase.initializeApp(environment.firebase);
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
