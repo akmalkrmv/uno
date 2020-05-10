6ba8b4739a6cb1636f19b0bf5b498c7cca91490b
akmalkrmv <akmalkarimov94@gmail.com>
2020-05-10 12:49:01 +0300

    change types for snapshot changes

--- src/app/services/repository/base-firestore.service.ts
+++ src/app/services/repository/base-firestore.service.ts
@@ -12,34 +12,29 @@ import { map, tap } from 'rxjs/operators';
 export class BaseFirestoreService {
   protected collectionChanges<T>(
     collection: AngularFirestoreCollection<T>,
-    changeType?: DocumentChangeType,
+    changeTypes?: DocumentChangeType[],
     logItems = false,
     logChanges = false
   ): Observable<T[]> {
-    return collection
-      .snapshotChanges()
-      .pipe(
-        tap((changes: DocumentChangeAction<T>[]) => {
-          logChanges && console.log(`${collection.ref.path}`, changes);
-        })
-      )
-      .pipe(
-        map((changes: DocumentChangeAction<T>[]) =>
-          changes
-            .filter((change: DocumentChangeAction<T>) =>
-              changeType ? change.type == changeType : true
-            )
-            .map((change: DocumentChangeAction<T>) => ({
-              ...(change.payload.doc.data() as T),
-              id: change.payload.doc.id,
-            }))
-        )
-      )
-      .pipe(
-        tap((items: T[]) => {
-          logItems && console.log(`${collection.ref.path}`, items);
-        })
-      );
+    return collection.snapshotChanges(changeTypes).pipe(
+      tap(() => console.log(`list path: ${collection.ref.path}`)),
+      tap((changes: DocumentChangeAction<T>[]) => {
+        logChanges &&
+          console.log(
+            `${collection.ref.path}`,
+            changes.map((change) => change.type)
+          );
+      }),
+      map((changes: DocumentChangeAction<T>[]) =>
+        changes.map((change: DocumentChangeAction<T>) => ({
+          ...(change.payload.doc.data() as T),
+          id: change.payload.doc.id,
+        }))
+      ),
+      tap((items: T[]) => {
+        logItems && console.log(`${collection.ref.path}`, items);
+      })
+    );
   }
 
   protected documentChanges<T>(
@@ -47,25 +42,20 @@ export class BaseFirestoreService {
     logItems = false,
     logChanges = false
   ): Observable<T> {
-    return document
-      .snapshotChanges()
-      .pipe(
-        tap((change: Action<DocumentSnapshot<T>>) => {
-          logChanges &&
-            console.log(`${document.ref.path} ${change.type}`, change);
-        })
-      )
-      .pipe(
-        map((change: Action<DocumentSnapshot<T>>) => ({
-          ...(change.payload.data() as T),
-          id: change.payload.id,
-        }))
-      )
-      .pipe(
-        tap((item: T) => {
-          logItems && console.log(`${document.ref.path}`, item);
-        })
-      );
+    return document.snapshotChanges().pipe(
+      tap(() => console.log(`doc path: ${document.ref.path}`)),
+      tap((change: Action<DocumentSnapshot<T>>) => {
+        logChanges &&
+          console.log(`${document.ref.path} ${change.type}`);
+      }),
+      map((change: Action<DocumentSnapshot<T>>) => ({
+        ...(change.payload.data() as T),
+        id: change.payload.id,
+      })),
+      tap((item: T) => {
+        logItems && console.log(`${document.ref.path}`, item);
+      })
+    );
   }
 
   protected addToCollection<T>(
