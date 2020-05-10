4cbe2a573caae22537ea5241c0dc6e235f0ddb29
akmalkrmv <akmalkarimov94@gmail.com>
2020-05-10 23:06:16 +0300

    cleanup

--- src/app/services/connection/ice-candidate.service.ts
+++ src/app/services/connection/ice-candidate.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { first, catchError, tap } from 'rxjs/operators';
+import { first, tap } from 'rxjs/operators';
 import { of } from 'rxjs';
 
 import { Connection, IceCandidate, User } from '@models/index';
