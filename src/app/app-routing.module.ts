66b97287c4a99d89d3a8dd37fc578dba61049bda
akmalkrmv <akmalkarimov94@gmail.com>
2020-05-11 20:56:19 +0300

    publish: home page is rooms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,6 @@ import { RoomComponent } from './modules/video-chat/components/room/room.compone
 import { PlayComponent } from './modules/bura/components/play/play.component';
 import { SigninComponent } from './modules/auth/signin/signin.component';
 import { LayoutComponent } from './componentes/layout/layout.component';
-import { HomeComponent } from './componentes/home/home.component';
 import { NameComponent } from './modules/auth/name/name.component';
 import { RoomListComponent } from './modules/video-chat/components/room-list/room-list.component';
 import { FriendsPageComponent } from './modules/video-chat/pages/friends-page/friends-page.component';
@@ -20,12 +19,12 @@ const routes: Routes = [
     component: LayoutComponent,
     canActivate: [AuthGuard],
     children: [
-      { path: '', component: HomeComponent },
       { path: 'friends', component: FriendsPageComponent },
       { path: 'rooms', component: RoomListComponent },
       { path: 'room/create', component: CreateRoomPageComponent },
       { path: 'room/:id', component: RoomComponent },
       { path: 'game/:id', component: PlayComponent },
+      { path: '', redirectTo: 'rooms', pathMatch: 'full' },
     ],
   },
   { path: 'login', component: SigninComponent },
