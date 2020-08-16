3511a3f4a662ba2e5589bf57e69846a18a86d570
akmalkrmv <akmalkarimov94@gmail.com>
2020-08-16 16:12:24 +0300

    service workers

--- src/combined-workers.js
+++ src/combined-workers.js
@@ -1,2 +1,5 @@
 importScripts('/uno/ngsw-worker.js');
 importScripts('/uno/firebase-messaging-sw.js');
+
+importScripts('/ngsw-worker.js');
+importScripts('/firebase-messaging-sw.js');
--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,9 @@ platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .then(() => {
     if ('serviceWorker' in navigator) {
-      // environment.production
-      //   ? navigator.serviceWorker.register('/uno/combined-workers.js')
-      //   : navigator.serviceWorker.register('/combined-workers.js');
+      environment.production
+        ? navigator.serviceWorker.register('/uno/combined-workers.js')
+        : navigator.serviceWorker.register('/combined-workers.js');
     }
   })
   .catch((err) => console.error(err));
